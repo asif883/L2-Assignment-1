34a89d2856388071e341bb1de2e885984b761580
asif883 <asifiqbal95863@gmail.com>
2025-05-07 21:04:12 +0600

    Problem-1

---
+++ Solutions-File/problems.ts
@@ -0,0 +1,6 @@
+function formatString(input: string, toUpper: boolean = true): string {
+    return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+console.log(formatString("HELLO", false));
+console.log(formatString("hello", true));
